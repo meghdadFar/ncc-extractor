28357c0f0059b33f4696a8cd65fa7631902c56e3
meghdad <meghdad@mrspock.fritz.box>
2019-01-01 18:26:51 +0100

    Implement prediction_batch() for a more efficient prediction

--- main.py
+++ main.py
@@ -24,16 +24,21 @@ if __name__ == '__main__':
 
     predict_ncs = read_ncs(config['PATH']['PREDICT_COMPUNDS'])
 
-    # predict_ncs = predict_ncs[0:200]
+    predict_ncs = predict_ncs[0:1000]
 
-    model = train(train_ncs, predict_ncs, gensim_w2v_model, config)
+    model, criterion = train(train_ncs, predict_ncs, gensim_w2v_model, config)
 
-    scored_ncs = noncomp_error_score(predict_ncs, gensim_w2v_model, model)
+    scored_ncs = noncomp_error_score(predict_ncs, gensim_w2v_model, model, criterion)
 
     sorted_scored_ncs = dict(sorted(scored_ncs.items(), key=lambda kv: kv[1], reverse=True))
 
     write_score(sorted_scored_ncs, config['PATH']['OUTPUT']+'/reg_scores_ep' + config['TRAINING']['NUM_EPOCHS'] + '.txt')
 
+    
+    
+    
+    # Obsolete
+    
     # reg_score = regression_score(train_ncs, predict_ncs, gensim_w2v_model)
 
     # ranked_ncs = rank_with_score(predict_ncs, reg_score)
--- regression.py
+++ regression.py
@@ -8,6 +8,7 @@ import re
 import torch.nn.functional as F
 from torch.autograd import Variable
 import logging
+import tqdm
 from config import logging_config
 
 use_cuda = torch.cuda.is_available()
@@ -47,7 +48,8 @@ def get_vector(w, gensim_w2v_model, length, seed):
 
 def train_epoch(inp_batches, tar_batches, model, optimizer, criterion):
     avg_loss = 0
-    for i in range(0, inp_batches.shape[0]):
+    logging.info('Training batches')
+    for i in tqdm.tqdm(range(0, inp_batches.shape[0])):
         Y = tar_batches[i]
         if model_config.poly_degree > 1:
             X = get_poly_features(inp_batches[i], model_config.poly_degree)
@@ -61,12 +63,10 @@ def train_epoch(inp_batches, tar_batches, model, optimizer, criterion):
 def train_batch(inp_batch, tar_batch, model, optimizer, criterion):
     inp = Variable(torch.from_numpy(inp_batch))
     tar = Variable(torch.from_numpy(tar_batch))
-    mdl = model
     if use_cuda:
-        mdl = mdl.cuda()
         inp = inp.cuda()
         tar = tar.cuda()
-    out = mdl(inp.float())
+    out = model(inp.float())
     optimizer.zero_grad()
     loss = criterion(out.float(), tar.float())
     loss.backward()
@@ -83,6 +83,8 @@ def train(train_ncs, predict_ncs, gensim_w2v_model, config):
     input_size = X.shape[1] if model_config.poly_degree == 1 else  get_poly_features(inp_batches[0], model_config.poly_degree).shape[1]
     output_size = Y.shape[1]
     model = torch.nn.Linear(input_size, output_size)
+    if use_cuda:
+        model = model.cuda()
     optimizer = optim.SGD(model.parameters(), lr=float(config['TRAINING']['LEARNING_RATE']))
     criterion = torch.nn.SmoothL1Loss()
 
@@ -95,32 +97,96 @@ def train(train_ncs, predict_ncs, gensim_w2v_model, config):
                 logging.info('epoch '+str(ep) +'\tloss ' + str(epoch_loss))
     except KeyboardInterrupt:
         pass
-    return model
+    return model, criterion
 
 
 
-def predict(eval_set_ncs, gensim_w2v_model, model):
+def predict(ncs, gensim_w2v_model, model):
     scored_ncs = {}
-    X, Y = get_vectors(eval_set_ncs, gensim_w2v_model)
-    if model_config.poly_degree > 1:
-        X = get_poly_features(X, model_config.poly_degree)
-    for i in range(0, X.shape[0]):
-        inp = Variable(torch.from_numpy(X[i, :]))
-        tar = Variable(torch.from_numpy(Y[i, :]))
+    X, Y = get_vectors(ncs, gensim_w2v_model)
+
+    logging.info('Scoring compounds')
+
+    for nc in tqdm.tqdm(ncs):
+        head, modifier = re.split(' ', nc)
+        w1w2 = np.append(get_vector(head, gensim_w2v_model, model_config.input_vector_length, model_config.seed),
+                         get_vector(modifier, gensim_w2v_model, model_config.input_vector_length, model_config.seed))
+        compound = head + '_' + modifier
+        y = get_vector(compound, gensim_w2v_model, model_config.output_vector_length, model_config.seed)
+
+        y = y.reshape(1, -1)
+        w1w2 = w1w2.reshape(1, -1)
+
+        inp = get_poly_features(w1w2, model_config.poly_degree) if model_config.poly_degree > 1 else w1w2
+
+        inp = Variable(torch.from_numpy(np.array(inp)))
+        tar = Variable(torch.from_numpy(y))
+
+        # inp = Variable(torch.from_numpy(X[i, :]))
+        # tar = Variable(torch.from_numpy(Y[i, :]))
+
         if use_cuda:
             # TODO check of the model is not cuda throw exception
             inp = inp.cuda()
             tar = tar.cuda()
         loss = F.smooth_l1_loss(model(inp.float()), tar.float())
-        scored_ncs[eval_set_ncs[i]] = loss.data[0]
+        scored_ncs[nc] = loss.item() 
     return scored_ncs
 
 
-def noncomp_error_score(predict_ncs, gensim_w2v_model, model):
+
+
+# def predict_batch(ncs, gensim_w2v_model, model, criterion):
+
+#     # Prepare batches
+#     output_ncs = []
+#     output_scores = []
+#     start_index = 0
+
+#     X, Y = get_vectors(ncs, gensim_w2v_model)    
+#     inp_batches, tar_batches = create_batch(X, Y, model_config.batch_size)
+
+#     criterion = torch.nn.SmoothL1Loss(reduce=False, size_average=True)
+#     # for i in range(0, inp_batches.shape[0]):
+#     logging.info('Scoring batches')
+#     for i in tqdm.tqdm(range(0, inp_batches.shape[0])):
+#         Y = tar_batches[i]
+#         if model_config.poly_degree > 1:
+#             X = get_poly_features(inp_batches[i], model_config.poly_degree)
+#         else:
+#             X = inp_batches[i]
+        
+#         inp = Variable(torch.from_numpy(X))
+#         tar = Variable(torch.from_numpy(Y))
+
+#         if use_cuda:
+#             inp = inp.cuda()
+#             tar = tar.cuda()
+        
+#         out = model(inp.float())
+#         loss = criterion(out.float(), tar.float())
+
+#         print(loss.shape)
+
+#         end_index = start_index + model_config.batch_size 
+
+#         output_ncs.extend(ncs[start_index:end_index])
+#         output_scores.extend(loss.tolist())
+
+#         # print(start_index, len(output_ncs), len(output_scores))
+
+#         start_index +=  model_config.batch_size
+
+#     return output_ncs, output_scores
+
+
+def noncomp_error_score(predict_ncs, gensim_w2v_model, model, criterion):
+    # ncs, scores = predict(predict_ncs, gensim_w2v_model, model, criterion)
     scored_ncs = predict(predict_ncs, gensim_w2v_model, model)
-    for k, v in scored_ncs.items():
-        v = v[0].item()  # Get the value out of torch tensor
-        # v = np.add(v, 0.001)
-        # v = np.true_divide(1, v)
-        scored_ncs[k] = v
+    # for k, v in scored_ncs.items():
+    #     scored_ncs[k] = v + 0.001
+    # for i in range(len(ncs)):
+        # print(ncs[i])
+        # print(scores[i])
+        # scored_ncs[nc[i]] = scores[i] + 0.001
     return scored_ncs
